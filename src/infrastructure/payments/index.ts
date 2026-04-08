840fe027c2e0f64b0e16fbc8ba57c09951f73287
Top Magar <magartop150@gmail.com>
2026-04-08 19:20:30 +0545

    feat: PaymentProvider interface + ManualPaymentProvider (COD/bank transfer)

---
+++ src/infrastructure/payments/index.ts
@@ -0,0 +1,2 @@
+export { getPaymentProvider, setPaymentProvider, ManualPaymentProvider } from "./provider"
+export type { PaymentProvider, PaymentMethod, CreateOrderPayment, PaymentResult, ConfirmPaymentResult } from "./provider"
